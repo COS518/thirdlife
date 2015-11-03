0e581f025f20812363783b31dd34e35084189937
Daniel Suo <danielsuo@gmail.com>
2015-11-02 19:55:06 -0500

    Initial commit

---
+++ lib/LAStools/LASzip/build/src/CMakeFiles/laszip.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laszip.dir/arithmeticdecoder.cpp.o"
+  "CMakeFiles/laszip.dir/arithmeticencoder.cpp.o"
+  "CMakeFiles/laszip.dir/arithmeticmodel.cpp.o"
+  "CMakeFiles/laszip.dir/integercompressor.cpp.o"
+  "CMakeFiles/laszip.dir/lasindex.cpp.o"
+  "CMakeFiles/laszip.dir/lasinterval.cpp.o"
+  "CMakeFiles/laszip.dir/lasquadtree.cpp.o"
+  "CMakeFiles/laszip.dir/lasreaditemcompressed_v1.cpp.o"
+  "CMakeFiles/laszip.dir/lasreaditemcompressed_v2.cpp.o"
+  "CMakeFiles/laszip.dir/lasreadpoint.cpp.o"
+  "CMakeFiles/laszip.dir/lasunzipper.cpp.o"
+  "CMakeFiles/laszip.dir/laswriteitemcompressed_v1.cpp.o"
+  "CMakeFiles/laszip.dir/laswriteitemcompressed_v2.cpp.o"
+  "CMakeFiles/laszip.dir/laswritepoint.cpp.o"
+  "CMakeFiles/laszip.dir/laszip.cpp.o"
+  "CMakeFiles/laszip.dir/laszipper.cpp.o"
+  "CMakeFiles/laszip.dir/laszip_dll.cpp.o"
+  "liblaszip.pdb"
+  "liblaszip.dylib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laszip.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lib/octree/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/danielsuo/thirdlife/lib/octree/PotreeConverter/src/BINPointReader.cpp" "/Users/danielsuo/thirdlife/lib/octree/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/BINPointReader.cpp.o"
+  "/Users/danielsuo/thirdlife/lib/octree/PotreeConverter/src/GridCell.cpp" "/Users/danielsuo/thirdlife/lib/octree/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/GridCell.cpp.o"
+  "/Users/danielsuo/thirdlife/lib/octree/PotreeConverter/src/LASPointReader.cpp" "/Users/danielsuo/thirdlife/lib/octree/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/LASPointReader.cpp.o"
+  "/Users/danielsuo/thirdlife/lib/octree/PotreeConverter/src/LASPointWriter.cpp" "/Users/danielsuo/thirdlife/lib/octree/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/LASPointWriter.cpp.o"
+  "/Users/danielsuo/thirdlife/lib/octree/PotreeConverter/src/PTXPointReader.cpp" "/Users/danielsuo/thirdlife/lib/octree/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/PTXPointReader.cpp.o"
+  "/Users/danielsuo/thirdlife/lib/octree/PotreeConverter/src/PointAttributes.cpp" "/Users/danielsuo/thirdlife/lib/octree/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/PointAttributes.cpp.o"
+  "/Users/danielsuo/thirdlife/lib/octree/PotreeConverter/src/PotreeConverter.cpp" "/Users/danielsuo/thirdlife/lib/octree/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/PotreeConverter.cpp.o"
+  "/Users/danielsuo/thirdlife/lib/octree/PotreeConverter/src/PotreeWriter.cpp" "/Users/danielsuo/thirdlife/lib/octree/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/PotreeWriter.cpp.o"
+  "/Users/danielsuo/thirdlife/lib/octree/PotreeConverter/src/SparseGrid.cpp" "/Users/danielsuo/thirdlife/lib/octree/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/SparseGrid.cpp.o"
+  "/Users/danielsuo/thirdlife/lib/octree/PotreeConverter/src/main.cpp" "/Users/danielsuo/thirdlife/lib/octree/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/main.cpp.o"
+  "/Users/danielsuo/thirdlife/lib/octree/PotreeConverter/src/stuff.cpp" "/Users/danielsuo/thirdlife/lib/octree/build/PotreeConverter/CMakeFiles/PotreeConverter.dir/src/stuff.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../PotreeConverter/include"
+  "../PotreeConverter/../LAStools/LASzip/dll"
+  "/usr/local/include"
+  "../PotreeConverter/lib/rapidjson/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
