3e030bfb0e5ad19a53c5890d553959968577043b
Daniel Suo <danielsuo@gmail.com>
2015-11-12 11:25:59 -0500

    Fix up gulp serve

--- client/gulp/serve.js
+++ client/gulp/serve.js
@@ -1,48 +1,24 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
-var browserSync = require('browser-sync');
-var reload = require('browser-sync').reload;
-
-var BROWSER_SYNC_RELOAD_DELAY = 100;
-
-gulp.task('nodemon', function(callback) {
-  var called = false;
-
-  return nodemon({
-      script: 'server.js',
-      watch: ['server.js']
-    })
-    .on('start', function() {
-      if (!called) {
-        callback();
-      }
-      called = true;
-    })
-    .on('restart', function() {
-      console.log('restarted!');
-      setTimeout(function() {
-        reload({
-          stream: false //
-        });
-        console.log('browserSync')
-      }, BROWSER_SYNC_RELOAD_DELAY);
-    });
-});
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
 gulp.task('js', function() {
-  gulp.watch('./src/**/*.js', ['build', reload]);
+  console.log('Running js!');
 });
 
 gulp.task('html', function() {
-  gulp.watch('./examples/**/*.html', [reload]);
+  console.log('Running html!');
 });
 
-gulp.task('serve', ['js', 'html'], function() {
-
-  browserSync({
+gulp.task('serve', function() {
+  browserSync.init({
     server: {
       baseDir: ".",
       directory: true
     }
   });
+
+  gulp.watch('./src/**/*.js', ['js', reload]);
+  gulp.watch('./examples/**/*.html', ['html', reload]);
 });
